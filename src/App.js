21b2278fcdc9d1b9b8dd354317cb63cb87e95173
Caner Akin <35246326+canerakin111@users.noreply.github.com>
2022-04-18 18:41:13 +0000

    included image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-
+import React from "react";
 import './App.css';
+import restaurant from "./restaurant.jpg";
 
 function Header(props) {
   return (
     <header>
       <h1>{props.name}'s kitchen</h1>
     </header>
-  )
+  );
   
 }
 function Main(props) {
   return (
     <section>
       <p>We server the most {props.adjective} food</p>
+      <img src={restaurant} height={200} alt="bar table in a restaurant"/>
       <ul style={{textAlign: "left"}}>
         {props.dishes.map((dish) => (
           <li key={dish.id}>{dish.title}</li>
