a9675d6922efd759a2144e20022f5e19d5d42903
Caner Akin <35246326+canerakin111@users.noreply.github.com>
2022-04-18 21:47:43 +0000

    Test library

--- src/App.js
+++ src/App.js
@@ -4,31 +4,11 @@ import './App.css';
 // https://api.github.com/users/canerakin111
 
 
-function App({login}) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-    .then((response) => response.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-  }, [login]);
-
-  if (loading) return <h1>Loading...</h1>;
-  if(error)
-    return<pre>{JSON.stringfy(error, null, 2)}</pre>;
-  if(!data) return null;
+function App() {
   
     return (
       <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url}/>
+        <h1>Hello React Test</h1>
       </div>
     );
   }
