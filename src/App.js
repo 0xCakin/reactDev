0d68d39065730951cb08ceb52960aa8069f67a9b
Caner Akin <35246326+canerakin111@users.noreply.github.com>
2022-04-18 21:36:20 +0000

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
+// https://api.github.com/users/canerakin111
 
-function App() {
-  const [checked, toggle] = useReducer(
-    (checked) => !checked,
-    false
-  );
 
-  return (
-    <>
-      <input 
-        type="checkbox" 
-        value={checked} 
-        onChange={toggle}/>
-      
-    <p>{checked ? "checked" : "not checked"} </p>
-    </>  
-  );
-}
+function App({login}) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if(!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if(error)
+    return<pre>{JSON.stringfy(error, null, 2)}</pre>;
+  if(!data) return null;
+  
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url}/>
+      </div>
+    );
+  }
 
 export default App;
