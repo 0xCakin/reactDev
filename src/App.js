1bf2de459f8dbc7f532404533445db7eefc84342
Caner Akin <35246326+canerakin111@users.noreply.github.com>
2022-04-18 20:22:14 +0000

    use useEffect - set dynamic text bound to clicks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`it's ${secondary} around here!`)
+  })
   return (
     <>
-    <h1>Current emotion is {emotion}.</h1>
+    <h1>Current emotion is {emotion} and {secondary}.</h1>
     <button onClick={() => setEmotion("happy")}>Happy</button>
+    <button onClick={() => setSecondary("crabby")}>crabby</button>
     <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-    <button onClick={() => setEmotion("enthusiastic")}>Enthusiastic</button>
+    <button onClick={() => setSecondary("enthusiastic")}>Enthusiastic</button>
     </>
   );
 }
